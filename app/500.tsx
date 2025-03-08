a5ecf4ec7b7243b33afe920d95c829f6c5f2267a
Stanislaw Jarocki <s.jarocki354@gmail.com>
2025-03-08 11:29:46 +0100

    add 500 and 404 pages

---
+++ app/500.tsx
@@ -0,0 +1,7 @@
+export default function ServerError() {
+  return (
+    <div>
+      <h1>500 - Server-Side Error</h1>
+    </div>
+  );
+}
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import Image from "next/image";
 
 const NavBar: FC = () => {
@@ -12,12 +12,12 @@ const NavBar: FC = () => {
                 height={50}
             />
             <div className="flex gap-x-little-xl text-subtitle-s">
-                <Link href="#about" className="hover:underline">o firmie</Link>
-                <Link href="#projects" className="hover:underline">realizacje</Link>
-                <Link href="#service" className="hover:underline">serwis</Link>
-                <Link href="#partners" className="hover:underline">partnerzy</Link>
-                <Link href="#reviews" className="hover:underline">opinie</Link>
-                <Link href="#contact" className="hover:underline">kontakt</Link>
+                {/* <Link href="#about" className="hover:underline">o firmie</Link> */}
+                {/* <Link href="#projects" className="hover:underline">realizacje</Link> */}
+                {/* <Link href="#service" className="hover:underline">serwis</Link> */}
+                {/* <Link href="#partners" className="hover:underline">partnerzy</Link> */}
+                {/* <Link href="#reviews" className="hover:underline">opinie</Link> */}
+                {/* <Link href="#contact" className="hover:underline">kontakt</Link> */}
             </div>
         </nav>
     );
