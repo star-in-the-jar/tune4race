dacc95d55b68bd72108e0d64e7186a613e1f8aae
Stanisław Jarocki <s.jarocki354@gmail.com>
2025-03-08 10:55:15 +0100

    feature: :sparkles: set up tailwind config and add navbar

---
+++ app/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="flex flex-wrap justify-center items-center gap-6 row-start-3">
+            this is footer
+        </footer>
+    );
+}
---
+++ app/section.tsx
@@ -0,0 +1,8 @@
+export default function Section() {
+    return (
+        <section className="flex flex-col justify-center items-center bg-primary-gradient px-big-s py-little- h-big-s size-li xxs">
+            <h1 className="text-white text-4xl">This is a section</h1>
+            <p className="text-white text-xl">This is a section</p>
+        </section>
+    );
+}
