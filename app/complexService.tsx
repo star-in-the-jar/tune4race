43257f5b16ef23f8243da18d9afa706acb30bf92
Stanislaw Jarocki <s.jarocki354@gmail.com>
2025-03-16 13:36:56 +0100

    feature: :sparkles: add subsections to service section

--- app/complexService.tsx
+++ app/complexService.tsx
@@ -1,6 +1,7 @@
 import Section from "./components/section";
 import Subsection from "./components/subsection";
 import { FC } from "react";
+import Image from "next/image";
 
 const ServiceCard = ({ service }: { service: string }) => {
   return (
@@ -59,6 +60,26 @@ const ComplexService: FC = () => {
             ))}
           </div>
         </div>
+      </Subsection>
+      <Subsection
+        title="Stawiamy na Jakość"
+        className="px-little-m md:px-big-s"
+      >
+        <p className="py-little-xl text-clamp-text-s">
+          Wszystkie prace wykonujemy z dużą starannością, a części do budowy lub
+          do serwisu pochodzą od najlepszych firm na rynku.
+        </p>
+        <div className="gap-little-s md:gap-little-l grid grid-cols-3 w-full">
+          <Image src="service_part_tire.jpg" alt="Tire" width={800} height={400} className="rounded-md"/>
+          <Image src="service_part_rings.jpg" alt="Auto part" width={800} height={400} className="rounded-md"/>
+          <Image src="service_part_rim.jpg" alt="Rime" width={800} height={400} className="rounded-md"/>
+        </div>
+      </Subsection>
+      <Subsection title="10 Letnie Doświadczenie" className="px-little-m md:px-big-s">
+        <p className="py-little-xl text-clamp-text-s">
+            Dbamy o auta naszych klientów od lat i przez ten czas mieliśmy okację zobaczyć wiele oraz przekonać się jakie efekty dają różne podejścia. Zadbamy o wybór rozwiązań, na których można polegać.
+        </p>
+        <Image src="service_team.jpg" alt="Experience" width={800} height={400} className="rounded-md"/>
       </Subsection>
     </Section>
   );
